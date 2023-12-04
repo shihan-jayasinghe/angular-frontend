324b1c2c092a39a6827d829622e9de23625f412d
shihan jayasinghe <jashihan@gmail.com>
2023-12-04 22:38:46 +0530

    create-mployee route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import path from 'path';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 
 const routes: Routes = [
   {path:"employees", component: EmployeeListComponent},
+  {path:"create-employee", component: CreateEmployeeComponent},
   {path:'', redirectTo: 'employees', pathMatch: 'full'}
 ];
 
